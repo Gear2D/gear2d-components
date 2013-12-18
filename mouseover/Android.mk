0787965678a2f5171653449bf5ecc8754062fdf3
leo <leonardo.guilherme@gmail.com>
2013-12-17 23:34:36 -0200

    Adding android.mk to mouseover component

---
+++ mouseover/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+SDL_PATH := ../SDL
+SDL_IMAGE_PATH := ../SDL2_image
+GEAR2D_PATH := ../gear2d
+LOCAL_MODULE := $(notdir $(LOCAL_PATH))
+LOCAL_CPPFLAGS := -std=c++11 -fexceptions -DLOGTRACE
+
+LOCAL_SRC_FILES := \
+  $(subst $(LOCAL_PATH)/,, \
+  $(wildcard $(LOCAL_PATH)/*.cc))
+
+LOCAL_STATIC_LIBRARIES := SDL2_static gear2d_static
+
+include $(BUILD_STATIC_LIBRARY)
