e9f73755654d26a53fee3a636bf4af7b6082995d
leo <leonardo.guilherme@gmail.com>
2014-01-20 18:26:29 -0200

    Static library for android with components

--- Android.mk
+++ Android.mk
@@ -1,1 +1,10 @@
+GEAR2D_COMPONENTS :=
+  
 include $(call all-subdir-makefiles)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := gear2d_components
+LOCAL_WHOLE_STATIC_LIBRARIES := $(GEAR2D_COMPONENTS)
+
+include $(BUILD_STATIC_LIBRARY)
--- touch/Android.mk
+++ touch/Android.mk
@@ -6,6 +6,7 @@ SDL_PATH := ../SDL
 GEAR2D_PATH := ../gear2d
 LOCAL_MODULE := $(notdir $(LOCAL_PATH))
 LOCAL_CPPFLAGS := -std=c++11 -fexceptions -DLOGTRACE
+GEAR2D_COMPONENTS += $(LOCAL_MODULE)
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,, \
